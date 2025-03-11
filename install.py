13688effbbbc6cc084ddd89657b7829b18782c8d
alefbemedia <110613191+AlefbeMedia@users.noreply.github.com>
2025-03-11 18:56:08 +0330

    Update install.py

--- install.py
+++ install.py
@@ -11,25 +11,13 @@ def show_progress():
         idx += 1
         time.sleep(0.1)
 
-def check_docker_installation():
-    try:
-        result = subprocess.run(['curl', '-fsSL', 'https://get.docker.com'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        return result.returncode == 0
-    except subprocess.CalledProcessError as e:
-        if e.returncode == 22 and "403" in e.stderr.decode():
-            return False
-        return True
-
-def apply_dns_settings():
-    subprocess.run(['sudo', 'bash', '-c', 'echo -e "nameserver 10.202.10.202\nnameserver 10.202.10.102" > /etc/resolv.conf'], check=True)
-
 def install_docker():
     subprocess.run('curl -fsSL https://get.docker.com | sh', shell=True, check=True)
 
 def configure_docker():
     os.makedirs('/etc/docker', exist_ok=True)
     with open('/etc/docker/daemon.json', 'w') as f:
-        f.write('{\n  "registry-mirrors": ["https://docker.arvancloud.ir"]\n}')
+        f.write('{\n  "registry-mirrors": ["https://docker.iranserver.com"]\n}')
     subprocess.run(['systemctl', 'daemon-reload'], check=True)
     subprocess.run(['systemctl', 'restart', 'docker'], check=True)
 
@@ -37,11 +25,8 @@ if __name__ == "__main__":
     progress_thread = threading.Thread(target=show_progress)
     progress_thread.daemon = True
     progress_thread.start()
-
-    if not check_docker_installation():
-        apply_dns_settings()
     
-    # install_docker()
+    install_docker()
     configure_docker()
 
     print("All tasks completed successfully.")
